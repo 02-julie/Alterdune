4cd2e1d458eeaac12205f50da061e6fa78b004b9
Erwanb10 <erwanberthelot78@gmail.com>
2026-04-07 15:58:22 +0200

    création classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-using namespace std;
-main(){
-    cout<<"hello world Julie"<<endl;
-    cout<<"bonjour"<<endl;
+#include "main.h"
+int main(){
+    std::cout<<"hello world Julie"<<std::endl;
+    std::cout<<"bonjour"<<std::endl;
+    return 0;
 };
