1c9dff26ad693276ae25d1c2c6d22f1e49598461
02-julie <97457670+02-julie@users.noreply.github.com>
2026-04-08 09:58:44 +0200

    items avec le fichier csv

--- joueur.cpp
+++ joueur.cpp
@@ -1,10 +1,57 @@
 #include "joueur.h"
+#include <fstream>
+#include <sstream>
 
 Joueur::Joueur(string nom,Item* items,int nb_items):Statistique( hp,  attaque, defense)
 {
     this->nom=nom;
     this->nb_items=nb_items;
     this->items=new Item[nb_items];
+    int compteur=0;
+    ifstream fichier("items.csv");
+    if (!fichier.is_open()) {
+        throw std::runtime_error("Erreur : impossible d'ouvrir le fichier ");
+    }
+    string ligne;
+    while(getline(fichier,ligne))
+    {
+        stringstream ss(ligne);
+        string nom;
+        string descriptif;
+        string streffet; 
+        string strquantite;
+        double effet;int quantite;
+        getline(ss,nom,',');
+        getline(ss,descriptif,',');
+        getline(ss,streffet,',');
+        getline(ss,strquantite,',');
+        try
+        {
+            double  effet=stod(streffet);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        try
+        {
+            int quantite=stoi(strquantite);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        Item it=Item::Item(compteur,nom,descriptif,effet,quantite);
+        items[compteur]=it;
+        compteur++;
+
+
+        
+        
+
+
+    }
+
     for(int i=0;i<nb_items;i++)
     {
         this->items[i]=items[i];
